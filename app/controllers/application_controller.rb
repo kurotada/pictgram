3266b6c010a20b0b1a0ca24802a1c4852d28f68d
Kurosawa <tadatadatanni.knss@me.com>
2021-12-23 05:51:26 +0000

    third commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,7 @@ class ApplicationController < ActionController::Base
    protect_from_forgery with: :exception
    add_flash_types :success, :info, :warning, :danger
    
-   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-   end
-
-   def logged_in?
-    !current_user.nil?
-   end
+   include CommonActions
    
    helper_method :current_user, :logged_in?
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,4 @@
 module ApplicationHelper
- def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
- end
+include CommonActions
 
-  def logged_in?
-    !current_user.nil?
-  end
-  
 end
