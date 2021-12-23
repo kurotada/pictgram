c718e4b71ccfc96a99101cf92409fd4801338e13
Kurosawa <tadatadatanni.knss@me.com>
2021-12-23 05:22:52 +0000

    third commit

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,20 +2,9 @@ class Topic < ApplicationRecord
   validates :user_id, presence: true
   validates :description, presence: true
   validates :image, presence: true
-  
-  
-  def extension_whitelist
-   %w(jpg jpeg png)
-  end
-  
-  def size_range
-   1..10.megabytes
-  end
 
 
-  
-  
-  
+
   belongs_to :user
   
   
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,4 +44,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+  
+  
+  def extension_whitelist
+   %w(jpg jpeg png)
+  end
+  
+  def size_range
+   1..10.megabytes
+  end
 end
